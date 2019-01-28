6a4023ecc2d72fd766cc8f517e460782771877f7
Sebastian Kippe <sebastian@kip.pe>
2019-01-28 18:46:19 +0800

    Fix webfinger connection errors not being shown
    The promise rejections were missing a return statement, so the rest of
    the function would execute and fail.

--- discovery.js
+++ discovery.js
@@ -30,11 +30,11 @@ let discovery = {
 
     webfinger.lookup(userAddress, function (err, response) {
       if (err) {
-        pending.reject(err.message);
+        return pending.reject(err.message);
       } else if ((typeof response.idx.links.remotestorage !== 'object') ||
                  (typeof response.idx.links.remotestorage.length !== 'number') ||
                  (response.idx.links.remotestorage.length <= 0)) {
-        pending.reject("WebFinger record for " + userAddress + " does not have remotestorage defined in the links section.");
+        return pending.reject("WebFinger record for " + userAddress + " does not have remotestorage defined in the links section.");
       }
       let rs      = response.idx.links.remotestorage[0];
       let authURL = rs.properties['http://tools.ietf.org/html/rfc6749#section-4.2'] || rs.properties['auth-endpoint'];
